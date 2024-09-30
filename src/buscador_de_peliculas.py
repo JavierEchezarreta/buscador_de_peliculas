f7719f8bb185ac501a7c10fcfe034d8c5f84f9d6
JavierEchezarreta <javier.echezarreta@alumnos.ispi4038.edu.ar>
2024-09-30 12:46:26 -0300

    Desarrollo de los test, implementación de pelicula y buscador

--- src/buscador_de_peliculas.py
+++ src/buscador_de_peliculas.py
@@ -1,14 +1,73 @@
+import json
+
 class Pelicula:
-    def __init__(self, poster, titulo, sinopsis, actores, puntacion, genero, duracion):
-        self.poster = poster
-        self.titulo = titulo
-        self.sinopsis = sinopsis
-        self.actores = actores
-        self.puntacion = puntacion
-        self.genero = genero
-        self.duracion = duracion
+    def __init__(self, poster, titulo, sinopsis, actores, puntuacion, genero, duracion):
+        self.__poster = poster
+        self.__titulo = titulo
+        self.__sinopsis = sinopsis
+        self.__actores = actores
+        self.__puntacion = puntuacion
+        self.__genero = genero
+        self.__duracion = duracion
+
+    def titulo(self):
+        return self.__titulo
+
+    def genero(self):
+        return self.__genero
+
+    def actores(self):
+        return self.__actores
 
 class BuscadorPeliculas:
     def __init__(self):
-        self.catalogo = []
+        self.__catalogo = []
 
+    def agregar_pelicula(self, poster, titulo, sinopsis, actores, puntuacion, genero, duracion):
+        self.__catalogo.append(Pelicula(poster, titulo, sinopsis, actores, puntuacion, genero, duracion))
+
+    def cantidad_peliculas(self):
+        return len(self.__catalogo)
+
+    def importar_peliculas(self, archivo):
+        try:
+            with open(archivo, 'r') as archivo_json:
+                datos = json.load(archivo_json)
+                for pelicula in datos:
+                    poster = pelicula['poster']
+                    titulo = pelicula['titulo']
+                    sinopsis = pelicula['sinopsis']
+                    actores = pelicula['actores']
+                    puntuacion = pelicula['puntuacion']
+                    genero = pelicula['genero']
+                    duracion = pelicula['duracion']
+                    self.agregar_pelicula(poster, titulo, sinopsis, actores, puntuacion, genero, duracion)
+        except (FileNotFoundError, json.JSONDecodeError) as e:
+            print(f"Error al importar el archivo: {e}")
+
+    def buscar_pelicula_por_titulo(self, titulo):
+        for pelicula in self.__catalogo:
+            if pelicula.titulo() == titulo:
+                return pelicula
+
+    def buscar_pelicula_por_genero(self, genero):
+        for pelicula in self.__catalogo:
+            if pelicula.genero() == genero:
+                return pelicula
+
+    def buscar_peliculas_comunes_por_actores(self, actor1, actor2):
+        peliculas = []
+        for pelicula in self.__catalogo:
+            if actor1 in pelicula.actores() and actor2 in pelicula.actores():
+                peliculas.append(pelicula.titulo())
+        return peliculas
+
+    def buscar_actores(self):
+        actores = set()
+        for pelicula in self.__catalogo:
+            actores_pelicula = pelicula.actores()
+            if isinstance(actores_pelicula, list):
+                actores.update(actores_pelicula)
+            elif isinstance(actores_pelicula, str):
+                actores.add(actores_pelicula)
+        return list(actores)
--- tests/tests_buscador_de_peliculas.py
+++ tests/tests_buscador_de_peliculas.py
@@ -5,30 +5,43 @@ from src.buscador_de_peliculas import BuscadorPeliculas
 def buscador():
     return BuscadorPeliculas()
 
-def test_agregar_pelicula_al_catalogo(buscador):
-    pass
+def test_agregar_pelicula(buscador):
 
-def test_agregar_pelicula_al_catalogo_desde_archivo_externo(buscador):
-    pass
+    buscador.agregar_pelicula('poster', 'titulo', 'sinopsis', 'actores', 'puntuacion', 'genero', 'duracion')
+    assert buscador.cantidad_peliculas() == 1
+
+def test_importar_peliculas_desde_archivo(buscador):
+
+    buscador.importar_peliculas('archivo.json')
+    assert buscador.cantidad_peliculas() == 2
 
 def test_buscar_pelicula_por_titulo(buscador):
-    pass
 
-def test_buscar_pelicula_por_anio_de_lanzamiento(buscador):
-    pass
+    buscador.agregar_pelicula('poster', 'El secreto de sus ojos', 'sinopsis', 'actores', 'puntuacion', 'genero', 'duracion')
+    pelicula = buscador.buscar_pelicula_por_titulo('El secreto de sus ojos')
+    assert pelicula.titulo() == 'El secreto de sus ojos'
 
 def test_buscar_pelicula_por_genero(buscador):
-    pass
 
-def test_buscar_peliculas_comunes_a_dos_actores(buscador):
-    pass
+    buscador.agregar_pelicula('poster', 'titulo', 'sinopsis', 'actores', 'puntuacion', 'drama', 'duracion')
+    pelicula = buscador.buscar_pelicula_por_genero('drama')
+    assert pelicula.genero() == 'drama'
 
-def test_mostrar_informacion_de_la_pelicula(buscador):
-    pass
+def test_buscar_peliculas_comunes_por_actores(buscador):
+
+    buscador.agregar_pelicula('poster', 'El secreto de sus ojos', 'sinopsis', ['Francella', 'Darin'], 'puntuacion', 'genero', 'duracion')
+    buscador.agregar_pelicula('poster', 'Nuevo Reinas', 'sinopsis', ['Pauls', 'Darin'], 'puntuacion', 'genero','duracion')
+    buscador.agregar_pelicula('poster', 'Granizo', 'sinopsis', ['Francella', 'Peña'], 'puntuacion', 'genero','duracion')
+    buscador.agregar_pelicula('poster', 'Esperando la carroza', 'sinopsis', ['Francella', 'Darin'], 'puntuacion','genero', 'duracion')
+    peliculas = buscador.buscar_peliculas_comunes_por_actores('Francella', 'Darin')
+    assert "El secreto de sus ojos" in peliculas
+    assert "Esperando la carroza" in peliculas
 
 def test_buscar_actores(buscador):
-    pass
 
-
-
-
+    buscador.agregar_pelicula('poster', 'El secreto de sus ojos', 'sinopsis', ['Francella', 'Darin'], 'puntuacion','genero', 'duracion')
+    buscador.agregar_pelicula('poster', 'Titanic', 'sinopsis', 'Di Caprio', 'puntuacion','genero', 'duracion')
+    actores = buscador.buscar_actores()
+    assert 'Francella' in actores
+    assert 'Darin' in actores
+    assert 'Di Caprio' in actores
